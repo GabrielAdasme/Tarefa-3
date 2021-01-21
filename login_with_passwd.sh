172c72c62cb6c61787131b460555c4d48c6a4efb
GabrielAdasme <77810526+GabrielAdasme@users.noreply.github.com>
2021-01-21 19:07:51 -0300

    Create login_with_passwd.sh

---
+++ login_with_passwd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# Como utilizar:
+# ./seuscript.sh -u usuario -p senha -s host
+
+
+# Setar variaveis em targ para ordenar entradas
+while getopts "u:p:s:" OPTION
+do
+        case $OPTION in
+
+                u) USER=$OPTARG ;;
+                p) PASS=$OPTARG ;;
+                s) HOST=$OPTARG ;;
+        esac
+done
+
+
+# Passagem de parametros para usuario
+if [ "$USER" ] && [ "$PASS" ] && [ "$HOST" ]; then
+
+    clear
+    echo "Conectando ao server... $HOST"
+    sshpass -p $PASS ssh $USER'@'$HOST
+
+fi
